45d701e8b3fa8b7d3c43a0a3296b78805489935c
Renat Singatullin <ren-sing@yandex.ru>
2024-04-07 10:34:59 +0300

    Добавлено решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+int[] CreateNewMas(int num1, int[] mas) {
+    if(num1 == -1) {return mas;}
+    mas[num1] = new Random().Next(1,10);
+    Console.Write($"{mas[num1]} ");
+    int[] res = CreateNewMas((num1 - 1), mas);
+    return res;
+}
+
+int[] RotateMas(int num1, int length, int[] mas) {
+    if(num1 == length) {return mas;}
+    Console.Write($"{mas[num1]} ");
+    int[] res = RotateMas((num1 + 1), length, mas);
+    return res;
+}
+
+int[] chisla = new int [10];
+Console.WriteLine(chisla.Length);
+int[] new_mas = CreateNewMas((chisla.Length-1), chisla);
+Console.WriteLine();
+RotateMas(0, (new_mas.Length), new_mas);
